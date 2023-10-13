c2c65547e86cff24060d64d3070251e7c060492e
boboybld <Boboybld9@gmail.com>
2023-10-13 17:09:46 +0200

    Counter application 13-10-2023

--- app/src/main/java/com/devapp/counterapp/MainActivity.java
+++ app/src/main/java/com/devapp/counterapp/MainActivity.java
@@ -3,12 +3,35 @@ package com.devapp.counterapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button click_me;
+    TextView counter_view;
+
+    int counter_number = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        click_me = findViewById(R.id.click_me_btn);
+        counter_view = findViewById(R.id.counter_view);
+
+        click_me.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter_view.setText("" +increase());
+            }
+        });
+
+
+    }
+
+    private int increase(){
+        return ++counter_number;
     }
 }
